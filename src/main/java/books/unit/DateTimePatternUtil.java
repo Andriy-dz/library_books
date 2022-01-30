3a98b2bc257324f554aac1d0df0ce84b6802255b
Andriy-dz <andriydzdz@gmail.com>
2022-01-30 13:18:43 +0200

    updated reabme file

---
+++ src/main/java/books/unit/DateTimePatternUtil.java
@@ -0,0 +1,6 @@
+package schedule.unit;
+
+public class DateTimePatternUtil {
+    public static final String DATE_TIME_PATTERN = "dd.MM.yyyy HH:mm";
+    public static final String DATE_PATTERN = "dd.MM.yyyy";
+}
